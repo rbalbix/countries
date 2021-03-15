5086b31f9cce33c330653e223a2e9b4400892d93
Ricardo Balbi <rbalbi@gmail.com>
2021-03-15 18:27:29 -0300

    feat(components): components organizing

---
+++ src/components/CountryCard.tsx
@@ -0,0 +1,5 @@
+import styles from '../styles/components/CountryCard.module.css';
+
+export function CountryCard() {
+  return <div className={styles.countryCardContainer}>Country Card.</div>;
+}
---
+++ src/components/Search.tsx
@@ -0,0 +1,5 @@
+import styles from '../styles/components/Search.module.css';
+
+export function Search() {
+  return <div className={styles.searchContainer}>Search Component.</div>;
+}
