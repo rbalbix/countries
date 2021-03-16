009c1976a8c14b1ce4e00849185dbb44ed883f5d
Ricardo Balbi <rbalbi@gmail.com>
2021-03-16 16:07:41 -0300

    feat(layouts): layout improvements

--- src/components/CountryCard.tsx
+++ src/components/CountryCard.tsx
@@ -1,5 +1,32 @@
 import styles from '../styles/components/CountryCard.module.css';
 
 export function CountryCard() {
-  return <div className={styles.countryCardContainer}>Country Card.</div>;
+  return (
+    <div className={styles.countryCardContainer}>
+      <header>
+        <h1>Brasil</h1>
+        <img src='/country-flag.svg' alt='Country Flag' />
+      </header>
+      <main>
+        <div>
+          <p className='countryLabel'>Capital</p>
+          <p className='countryData'>Brasília</p>
+        </div>
+        <div>
+          <p className='countryLabel'>Official Language</p>
+          <p className='countryData'>Português</p>
+        </div>
+        <div>
+          <p className='countryLabel'>Area</p>
+          <p className='countryData'>
+            8,515,767 km<sup>2</sup>
+          </p>
+        </div>
+        <div>
+          <p className='countryLabel'>Population</p>
+          <p className='countryData'>206,135,893</p>
+        </div>
+      </main>
+    </div>
+  );
 }
--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,5 +1,12 @@
 import styles from '../styles/components/Search.module.css';
 
 export function Search() {
-  return <div className={styles.searchContainer}>Search Component.</div>;
+  return (
+    <div className={styles.searchContainer}>
+      <input type='text' name='search' id='search' />
+      <button>
+        <img src='/search-logo.svg' alt='Search Logo' />
+      </button>
+    </div>
+  );
 }
